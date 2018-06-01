6cfe5c3704b56e34648b716714984920bf86686a
jia lei <sfw123817@qq.com>
2018-06-01 10:58:13 +0800

    init checkin

--- word_vec/pre_handel_jieaba.py
+++ word_vec/pre_handel_jieaba.py
@@ -7,9 +7,10 @@ with open(filePath, encoding="utf-8") as fileTrainRaw:
     fileTrainRead = [line[9:-11] for line in fileTrainRaw.readlines()]
 
 fileTrainSeg = []
+# fileTrainSeg.append(" ".join(list(jieba.cut(fileTrainRead[1], cut_all=False))))
 for line in fileTrainRead:
     fileTrainSeg.append(" ".join(list(jieba.cut(line, cut_all=False))))
 
-with open(fileSegWordDonePath, 'wb') as fw:
+with open(fileSegWordDonePath, 'w', encoding='utf-8') as fw:
     for line in fileTrainSeg:
         fw.write(line + "\n")
--- word_vec/word_vec.py
+++ word_vec/word_vec.py
@@ -1,3 +1,4 @@
+import genericpath
 from gensim.models import word2vec
 
 word2vec.Word2Vec(sentences, size=200)
