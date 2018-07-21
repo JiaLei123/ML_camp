638ab27536db41e12ad17e1bb084998982fb6c07
JiaLei <sfw123817@gmail.com>
2018-07-21 22:51:17 +0800

    add word count

---
+++ daguan/counter_words.py
@@ -0,0 +1,35 @@
+from collections import Counter
+import pandas as pd
+import matplotlib.pyplot as plt
+from pylab import mpl
+
+
+
+data_path = "E:\\ML_learning\\Daguan\\data\\train_set.csv"
+train_data = pd.read_csv(data_path)
+
+article = train_data["article"]
+f = lambda x: x.split(" ")
+article_list = article.apply(f)
+
+word_counts = Counter()
+for line in article_list:
+    word_counts.update(line)
+
+# i = 1
+# with open(data_path) as f:
+#     for line in f:
+#         # print("add new batch %d" % i)
+#         # i += 1
+#         words_line = line.strip().split(',')[1]
+#         word_counts.update(words_line.strip().split(' '))
+
+# print(word_counts)
+
+counter_list = sorted(word_counts.items(), key=lambda x: x[1], reverse=True)
+
+label = list(map(lambda x: x[0], counter_list[:20]))
+value = list(map(lambda y: y[1], counter_list[:20]))
+
+plt.bar(range(len(value)), value, tick_label=label)
+plt.show()
