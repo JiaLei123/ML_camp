126d3d1380c40b9b824d6784760c19fc031426a4
jia lei <sfw123817@qq.com>
2018-07-26 19:09:18 +0800

    update batch size define

---
+++ daguan/general_train_test_data.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+data_path = "D:\\ML_learning\\Daguan\\data\\train_set.csv"
+total_data = pd.read_csv(data_path)
+total_useful_data = total_data.loc[:,["article", "class"]]
+total_useful_data = total_useful_data.sample(frac=1)
+total_len = len(total_useful_data.index)
+train_data_len = int(total_len * 0.6)
+test_data = total_len - train_data_len
+train_data = total_useful_data[0:train_data_len]
+test_data = total_useful_data[train_data_len:total_len + 1]
+train_data.to_csv( "D:\\ML_learning\\Daguan\\data\\train_data.csv")
+test_data.to_csv( "D:\\ML_learning\\Daguan\\data\\test_data.csv")
--- daguan/rnn_gluon_daguan_w2v.py
+++ daguan/rnn_gluon_daguan_w2v.py
@@ -4,13 +4,11 @@ import time
 import numpy as np
 import mxnet as mx
 from mxnet import gluon, autograd
-from mxnet.gluon import nn, rnn
+from mxnet.gluon import nn, rnn, data as gdata
 import zipfile
 from MXnet import utils
 from gensim.models import word2vec
-
-with zipfile.ZipFile('../data/ptb.zip', 'r') as zin:
-    zin.extractall('../data/')
+import pandas as pd
 
 
 class Dictionary(object):
@@ -64,7 +62,7 @@ class Corpus(object):
 
 
 class RNNModel(gluon.Block):
-    def __init__(self, mode, vocab_size, embed_dim, hidden_dim, num_layers, w2v_vec, drop_out=0.5, **kwargs):
+    def __init__(self, mode, embed_dim, hidden_dim, num_layers, w2v_vec, drop_out=0.5, **kwargs):
         super(RNNModel, self).__init__(**kwargs)
         with self.name_scope():
             self.drop = nn.Dropout(drop_out)
@@ -82,29 +80,23 @@ class RNNModel(gluon.Block):
             else:
                 raise ValueError("Invalid Mode")
 
-            self.decoder = nn.Dense(vocab_size, in_units=hidden_dim)
+            self.decoder = nn.Dense(19, in_units=hidden_dim)
             self.hidden_dim = hidden_dim
             self.w2v_vec = w2v_vec
 
     def get_vec(self,inputs):
-        step, batch = inputs.shape
-        input_node = inputs.reshape((-1,))
         input_vec = []
-        for idx, item in enumerate(input_node):
+        for word in enumerate(inputs):
             try:
-                word = corpus.dictionary.idx_to_word[item.asscalar()]
                 input_vec.append(self.w2v_vec.wv[word])
             except:
                 input_vec.append(np.random.uniform(-0.25, 0.25, self.w2v_vec.vector_size))
-        return mx.nd.array(input_vec).reshape((step, batch, -1))
+        return mx.nd.array(input_vec).reshape((len(inputs), 1, -1))
 
     def forward(self, inputs, state):
         input_node = self.get_vec(inputs)
         emb = self.drop(input_node)
-        try:
-            output, state = self.rnn(emb, state)
-        except:
-            pass
+        output, state = self.rnn(emb, state)
         output = self.drop(output)
         decoded = self.decoder(output.reshape((-1, self.hidden_dim)))
         return decoded, state
@@ -112,50 +104,20 @@ class RNNModel(gluon.Block):
     def begin_state(self, *args, **kwargs):
         return self.rnn.begin_state(*args, **kwargs)
 
+def get_batch(source, label, i):
+    data = source[i]
+    target = label[i]
+    return data, target
 
-model_name = 'rnn_relu'
-embed_dim = 100
-hidden_dim = 100
-num_layers = 2
-lr = 1
-clipping_norm = 0.2
-epochs = 10
-batch_size = 32
-num_steps = 5
-dropout_rate = 0.2
-eval_period = 500
-
-context = utils.try_gpu()
-
-
-def batchify(data, batch_size):
-    num_batches = data.shape[0] // batch_size
-    data = data[: num_batches * batch_size]
-    data = data.reshape((batch_size, num_batches)).T
-    return data
-
-
-data = '../data/ptb/ptb.'
-corpus = Corpus(data)
-vocab_size = len(corpus.dictionary)
-print(vocab_size)
-
-train_data = batchify(corpus.train, batch_size).as_in_context(context)
-val_data = batchify(corpus.valid, batch_size).as_in_context(context)
-test_data = batchify(corpus.test, batch_size).as_in_context(context)
-
-model = RNNModel(model_name, vocab_size, embed_dim, hidden_dim, num_layers, corpus.w2v, dropout_rate)
-model.collect_params().initialize(mx.init.Xavier(), ctx=context)
-
-trainer = gluon.Trainer(model.collect_params(), 'sgd', {'learning_rate': lr, 'momentum': 0, 'wd': 0})
-loss = gluon.loss.SoftmaxCrossEntropyLoss()
-
-
-def get_batch(source, i):
-    seq_len = min(num_steps, source.shape[0] - 1 - i)
-    data = source[i: i + seq_len]
-    target = source[i + 1: i + 1 + seq_len]
-    return data, target.reshape((-1,))
+def get_data_iter(path, batch_size):
+    total_data = pd.read_csv(path)
+    data = total_data["article"]
+    f = lambda x: x.split(" ")
+    data = data.apply(f)
+    label = total_data["class"]
+    # dataset = gdata.ArrayDataset(np.array(data), np.array(label))
+    # data_iter = gdata.DataLoader(dataset, batch_size, shuffle=True)
+    return data, label
 
 
 def detach(state):
@@ -166,17 +128,17 @@ def detach(state):
     return state
 
 
-def model_eval(data_source):
-    total_L = 0.0
-    ntotal = 0
-    hidden = model.begin_state(func=mx.nd.zeros, batch_size=batch_size, ctx=context)
-    for i in range(0, data_source.shape[0] - 1, num_steps):
-        data, target = get_batch(data_source, i)
-        output, hidden = model(data, hidden)
-        L = loss(output, target)
-        total_L += mx.nd.sum(L).asscalar()
-        ntotal += L.size
-    return total_L / ntotal
+# def model_eval(data_source):
+#     total_L = 0.0
+#     ntotal = 0
+#     hidden = model.begin_state(func=mx.nd.zeros, batch_size=batch_size, ctx=context)
+#     for i in range(0, data_source.shape[0] - 1, num_steps):
+#         data, target = get_batch(data_source, i)
+#         output, hidden = model(data, hidden)
+#         L = loss(output, target)
+#         total_L += mx.nd.sum(L).asscalar()
+#         ntotal += L.size
+#     return total_L / ntotal
 
 
 def train():
@@ -185,31 +147,56 @@ def train():
         start_time = time.time()
         hidden = model.begin_state(func=mx.nd.zeros, batch_size=batch_size, ctx=context)
 
-        for ibatch, i in enumerate(range(0, train_data.shape[0] - 1, num_steps)):
-            data, target = get_batch(train_data, i)
+        for i in range(len(label)):
+            X, y = get_batch(train_data, label, i)
             hidden = detach(hidden)
             with autograd.record():
-                output, hidden = model(data, hidden)
-                L = loss(output, target)
+                output, hidden = model(X, hidden)
+                output = output[-1].reshape((1, -1))
+                L = loss(output, mx.nd.array([y]))
                 L.backward()
             grads = [i.grad(context) for i in model.collect_params().values()]
             gluon.utils.clip_global_norm(grads, clipping_norm * num_steps * batch_size)
             trainer.step(batch_size)
             total_L += mx.nd.sum(L).asscalar()
 
-            if ibatch % eval_period == 0 and ibatch > 0:
-                cur_L = total_L / num_steps / batch_size / eval_period
-                print('[Epoch %d Batch %d] loss %.2f, perplexity %.2f' % (
-                    epoch + 1, ibatch, cur_L, math.exp(cur_L)))
-                total_L = 0.0
+            if i % eval_period == 0 and i > 0:
+                cur_L = total_L / i
+                print('[Epoch %d Batch %d] loss %.2f' % (epoch + 1, i, cur_L, ))
 
-        val_L = model_eval(val_data)
-        print('[Epoch %d] time cost %.2fs, validation loss %.2f, validation '
-              'perplexity %.2f' % (epoch + 1, time.time() - start_time, val_L,
-                                   math.exp(val_L)))
+        cur_L = total_L / len(label)
+        print('[Epoch %d loss %.2f' % (epoch + 1, cur_L))
 
 
-# model_eval(val_data)
-train()
-test_L = model_eval(test_data)
-print('Test loss %.2f, test perplexity %.2f' % (test_L, math.exp(test_L)))
+if __name__=="__main__":
+    model_name = 'rnn_relu'
+    embed_dim = 100
+    hidden_dim = 100
+    num_layers = 2
+    lr = 1
+    clipping_norm = 0.2
+    epochs = 10
+    batch_size = 1
+    num_steps = 1
+    dropout_rate = 0.2
+    eval_period = 20
+
+    context = utils.try_gpu()
+
+    train_data_path = "D:\\ML_learning\\Daguan\\data\\train_data.csv"
+    # test_data_path = ""
+    train_data, label = get_data_iter(train_data_path, batch_size)
+    # test_data_iter = get_data_iter(test_data_path, batch_size)
+    w2v = word2vec.Word2Vec.load("D:\\ML_learning\\Daguan\\data\\mymodel")
+
+    model = RNNModel(model_name, embed_dim, hidden_dim, num_layers, w2v, dropout_rate)
+    model.collect_params().initialize(mx.init.Xavier(), ctx=context)
+
+    trainer = gluon.Trainer(model.collect_params(), 'sgd', {'learning_rate': lr, 'momentum': 0, 'wd': 0})
+    loss = gluon.loss.SoftmaxCrossEntropyLoss()
+
+    # model_eval(val_data)
+    train()
+    # test_L = model_eval(test_data_iter)
+    # print('Test loss %.2f, test perplexity %.2f' % (test_L, math.exp(test_L)))
+
