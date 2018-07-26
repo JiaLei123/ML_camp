498982f1b10a951e7471c60856d69688c4e5908d
JiaLei <sfw123817@gmail.com>
2018-07-27 00:16:23 +0800

    add word2vec

--- word_vec/word_vec.py
+++ word_vec/word_vec.py
@@ -1,4 +1,13 @@
-import genericpath
 from gensim.models import word2vec
+import pandas as pd
 
-word2vec.Word2Vec(sentences, size=200)
+mopdelfilePath = 'E:\\ML_learning\\NLP_data\\model.bin'
+fileSegWordDonePath = 'E:\\ML_learning\\NLP_data\\corpusSegDone.txt'
+
+fileTrainRead = pd.read_csv(fileSegWordDonePath)
+train_sentences = pd.Series(fileTrainRead.iloc[:, 1])
+f = lambda x: str(x).split(" ")
+train_sentences = train_sentences.apply(f)
+
+model = word2vec.Word2Vec(train_sentences, size=300)
+model.save(mopdelfilePath)
